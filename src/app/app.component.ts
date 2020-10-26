acdd92409c4bb14b66c446b8de33e5dd8004d5da
Malashenko <dimaneopal@mail.ru>
2020-10-26 17:37:14 +0300

    Homework 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+ title='my-app';
+ firstOperand:number;
+ secondNumber:number;
+ operator:string;
+ result:number;
+ cError:string;
+ doCalc(){
+    if(this.firstOperand!=null&&this.secondNumber!=null&&this.operator!=null){
+      if(this.operator=="-")
+      {
+        this.result= this.firstOperand-this.secondNumber;
+       this.cError="нет ошибки" ;
+      }
+      else if(this.operator=="+"){
+        this.result= this.firstOperand+this.secondNumber;
+        this.cError="нет ошибки" ;
+      }
+      else if(this.operator=="/"){
+        if(this.secondNumber==0){
+          this.cError="на 0 делить нельзя";
+          this.result=null;
+        }
+        else{
+          this.result=this.firstOperand/this.secondNumber;
+          this.cError="нет ошибки";
+        }
+      }else if(this.operator=="*"){
+        this.result=this.firstOperand*this.secondNumber;
+        this.cError="нет ошибки";
+
+        }
+      }else{
+        if(this.firstOperand==null){
+          this.cError="нет первого операнта";
+        }
+        else if(this.secondNumber==null){
+          this.cError="нет второго операнта";
+        }
+        else if(this.operator==null){
+          this.cError="нет оператора действий ";
+        }
+        
+
+      }
+
+  }
 }
+
