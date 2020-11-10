617afa242308568b4b3d90adbec340e779d394bc
Malashenko <dimaneopal@mail.ru>
2020-11-10 22:27:02 +0300

    оаоаооао

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,51 @@ export class AppComponent {
   title(title: any) {
     throw new Error('Method not implemented.');
   }
+  firstOperand: number;
+  secondNumber: number;
+  operatoR:string;
+  operator: string;
+  result: boolean|string;
+  cError: string;
   
-}
+  doCalc() {
+    if (!this.operator) {
+      this.cError = "отсутсвует 2 оператор ";
+      this.result = "Error";
+      return;
+    }
+   
+
+    
+    else if (this.operatoR === "!") {
+      if (this.operator === "&&") {
+        this.result =  !Boolean(this.firstOperand && this.secondNumber);
+        this.cError = "aaaaa ";
+        return;
+
+       
+      }
+      else {
+        this.result = !Boolean(this.firstOperand || this.secondNumber);
+        this.cError = "aaa";
+        return;
+
+      }
+    }
+    else {
+      if (this.operator === "&&") {
+        this.result = Boolean(this.firstOperand && this.secondNumber);
+        this.cError = "aaaa";
+        return;
+
+      }
+      else {
+        this.result = Boolean(this.firstOperand || this.secondNumber);
+        this.cError = "aaaa";
+        return;
+
+      }
+  }
+  }
+  }
 
